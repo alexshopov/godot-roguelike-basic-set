670b063565da4dfaadc9d9fcbe8353c7f692608d
Alex Shopov <shopov.alex@gmail.com>
2025-07-17 21:40:18 -0400

    fixed Main formatting

--- Main.cs
+++ Main.cs
@@ -1,7 +1,9 @@
 using Godot;
 
-public partial class Main : Node3D {
-    public override void _Ready() {
+public partial class Main : Node3D
+{
+    public override void _Ready()
+    {
         GD.Print("Hello r/roguelikedev!");
     }
 }
